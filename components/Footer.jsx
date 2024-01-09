cee372d275e218f0a80cbd1676b32c8f3ce8ac6f
Balaji Upadhyay <91815309+Balajiupadhyay@users.noreply.github.com>
2024-01-09 16:08:46 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer>&#169; 2022 RoamWheels Balaji Upadhyay</footer>
+        <footer>&#169; RoamWheels Balaji Upadhyay</footer>
     )
 }
